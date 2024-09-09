33400c74d722afd64d1696b141110e47960321c4
Ahmed Alatawi <aalat001@gmail.com>
2024-09-09 04:12:26 -0400

    Removed eslint modules

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -12,8 +12,7 @@ export default function Spinner() {
           cx="12"
           cy="12"
           r="10"
-          stroke="currentColor"
-          stroke-width="4"
+          strokeWidth="4"
         ></circle>
         <path
           className="opacity-75"
